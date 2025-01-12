aa53133c29e7cfc6d660b24bfd8889a192d5639e
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-01-12 19:37:50 +0530

    Setup client

---
+++ client/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles.css';
+
+function App() {
+  return (
+    <div className="flex h-dvh items-center justify-center bg-gray-200">
+      <h1 className="text-6xl font-extrabold text-gray-800">Secure Share</h1>
+    </div>
+  );
+}
+
+export default App;
