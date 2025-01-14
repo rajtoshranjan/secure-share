0af3cb00d5bc45cc5f3f0496362d7dc246b24a17
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-01-14 10:47:23 +0530

    Update token expiry time and date

--- server/secure_share/settings.py
+++ server/secure_share/settings.py
@@ -173,8 +173,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
 # CORS.
