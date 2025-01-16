a3698856e64874014bbc4d5f794fc2e720d265a2
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-01-16 09:33:24 +0530

    Merge migrations

--- server/files/urls.py
+++ server/files/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import FilePermissionViewSet, FileShareLinkViewSet, FileViewSet
 
 router = DefaultRouter()
-router.register('shares', FilePermissionViewSet, basename='file-share')
+router.register('permissions', FilePermissionViewSet, basename='file-permission')
 router.register('links', FileShareLinkViewSet, basename='file-share-link')
 router.register('', FileViewSet, basename='file')
 
